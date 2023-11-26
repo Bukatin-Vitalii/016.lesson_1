23f302ea009093b1e14e522ceb8d091eaa202d2c
Vitalii Bukatin <bukatin93@gmail.com>
2023-11-26 20:06:30 +0100

    completed hw

---
+++ script.js
@@ -0,0 +1,129 @@
+const input = document.querySelector('.input-value');
+const submit = document.querySelector('.input-submit');
+const post = document.querySelector('.post__info');
+const errorWrap = document.querySelector('.error');
+const errorText = document.querySelector('.error__text');
+const comments = document.querySelector('.comments');
+const loader = document.querySelector('.loader');
+let inputValue;
+
+function showLoader() {
+	loader.classList.add('loader--active');
+}
+
+function hideLoader() {
+	loader.classList.remove('loader--active');
+}
+
+function getPosts() {
+	inputValue = input.value;
+	if (inputValue >= 1 && inputValue <= 100) {
+		showLoader();
+
+		const timeoutPromise = new Promise((_, reject) => {
+			setTimeout(() => {
+				reject('Request timed out');
+			}, 5000);
+		});
+
+		Promise.race([
+			fetch(`https://jsonplaceholder.typicode.com/posts/${input.value}`),
+			timeoutPromise,
+		])
+			.then((response) => response.json())
+			.then((data) => {
+				renderPost(data);
+			})
+			.catch((e) => {
+				hideLoader();
+				renderError(e);
+			})
+			.finally(() => {
+				hideLoader();
+			});
+	} else {
+		renderError('invalidValue');
+	}
+}
+
+function renderPost(data) {
+	post.classList.add('post__info--active');
+	post.innerHTML = `<h2 class="post__title" >${data.title}</h2><p class="post__text" >${data.body}</p><button class="post__button" id="btn-comment">Get comments</button>`;
+
+	inputValue = input.value;
+	input.value = '';
+
+	let btnComment = document.querySelector('#btn-comment');
+	btnComment.addEventListener('click', () => {
+		getComments(inputValue);
+	})
+}
+
+function getComments(id) {
+	showLoader();
+
+	const timeoutPromise = new Promise((_, reject) => {
+		setTimeout(() => {
+			reject('Request timed out');
+		}, 5000);
+	});
+
+	Promise.race([
+		fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`),
+		timeoutPromise,
+	])
+		.then((response) => response.json())
+		.then((data) => {
+			comments.classList.add('comments--active');
+			comments.innerHTML = '';
+			data.forEach((element) => {
+				comments.innerHTML += `<div class="comment"><h3 class="comment__title">${element.name}</h3><p class="comment__text">${element.body}</p></div>`;
+			});
+			hideLoader();
+		})
+		.catch((e) => {
+			hideLoader();
+			renderError(e);
+		})
+		.finally(() => {
+			hideLoader();
+		});
+}
+
+
+
+submit.addEventListener('click', () => {
+	getPosts();
+
+	comments.classList.remove('comments--active');
+	comments.innerHTML = '';
+});
+
+function renderError(error) {
+	errorWrap.classList.add('error--active');
+
+	post.classList.remove('post__info--active');
+	post.innerHTML = '';
+
+	comments.classList.remove('comments--active');
+	comments.innerHTML = '';
+
+	switch (error) {
+		case 'invalidValue':
+			input.value = '';
+			inputValue = null;
+			errorText.innerHTML = 'Enter a number from 1 to 100';
+			break;
+		case 'TypeError: Failed to fetch':
+			errorText.innerHTML = 'Error 404';
+			break;
+		default:
+			errorText.innerHTML = error;
+			break;
+	}
+
+
+	setTimeout(() => {
+		errorWrap.classList.remove('error--active');
+	}, 1700);
+}
